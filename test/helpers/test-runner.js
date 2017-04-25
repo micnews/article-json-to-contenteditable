aa7791bdd5383f9cf6f01e3c8c704751771122d1
ryanscottaudio <iamryancampbell@gmail.com>
2017-04-25 17:45:21 -0400

    Start trying to use jsdom in tests since enzyme's iframes never call onLoad

--- test/helpers/test-runner.js
+++ test/helpers/test-runner.js
@@ -1,9 +1,25 @@
 /* eslint-disable import/no-extraneous-dependencies */
 
-import test from 'tape-catch';
+import _test from 'tape-catch';
+import { jsdom } from 'jsdom';
 
-if (process.browser) {
-  test.onFinish(global.close);
-}
+const test = process.browser
+? (msg, opts, cb) => {
+  const { document: oldDoc, window: oldWin, navigator: oldNav } = global;
+
+  global.document = jsdom('');
+  global.window = global.document.defaultView;
+  global.window.document = global.document;
+  global.navigator = global.window.navigator;
+
+  _test.onFinish(() => {
+    global.document = oldDoc;
+    global.window = oldWin;
+    global.navigator = oldNav;
+    global.close();
+  });
+
+  _test(msg, opts, cb);
+} : _test;
 
 export default test;
